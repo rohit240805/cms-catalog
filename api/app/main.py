9f5024a31e0cf5dfebb073688fb14e53213a781b
rohit240805 <rohit086224@gmail.com>
2026-01-12 08:34:02 +0530

    Fix CORS for Netlify frontend

--- api/app/main.py
+++ api/app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
+        "https://chaishotsassignment.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
